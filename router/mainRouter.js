e8556f9e3178b84e1be74c162d5269ab16d9635b
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-18 12:04:22 +0900

    feat: Make jquery examples

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   let data = {
-    title: '유성균 만세',
-    html: '<h2>EJS에서 왔습니다</h2>',
+    title: "유성균 만세",
+    html: "<h2>EJS에서 왔습니다</h2>",
   };
-  res.render('main', { data });
+  res.render("main", { data });
 });
 
-router.post('/', async (req, res) => {
-  res.send('포스트 메인');
+router.post("/", async (req, res) => {
+  res.send("포스트 메인");
+});
+
+router.get("/ex", async (req, res) => {
+  res.render("exercise");
 });
 
 module.exports = router;
